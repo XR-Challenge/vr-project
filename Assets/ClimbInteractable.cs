fc8175890b0d86248ba7d99891f4470b677df928
Relax With My Horns <117208695+relaxwithmyhorns@users.noreply.github.com>
2022-11-05 13:41:48 -0600

    Revert "Creating climbing interaction script"
    This reverts commit 9f9d9c998f29363a065f793dea2f74e4892e0224.

--- Assets/ClimbInteractable.cs
+++ Assets/ClimbInteractable.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 
-public class ClimbInteractable : XRBaseInteractable
+public class ClimbInteractable : MonoBehaviour
 {
-    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
