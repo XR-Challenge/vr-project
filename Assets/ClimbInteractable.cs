9f9d9c998f29363a065f793dea2f74e4892e0224
Relax With My Horns <117208695+relaxwithmyhorns@users.noreply.github.com>
2022-11-05 13:41:34 -0600

    Creating climbing interaction script

--- Assets/ClimbInteractable.cs
+++ Assets/ClimbInteractable.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class ClimbInteractable : MonoBehaviour
+public class ClimbInteractable : XRBaseInteractable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
